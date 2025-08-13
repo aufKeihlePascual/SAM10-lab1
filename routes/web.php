cb2708811a309b557876bc9dbe7e262aa30c82f9
aufKeihlePascual <pascual.keihlediannegyraser@auf.edu.ph>
2025-08-13 21:24:37 +0800

    If using index.html instead of blade.php

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('index');
+// });
+
 Route::get('/', function () {
-    return view('index');
+    return response()->file(public_path('views/index.html'));
 });
